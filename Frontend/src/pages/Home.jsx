5988d59a876e4d7dd1ecef2730ea1f09e0b6cde7
Katleho14 <katlehomotloung394@gmail.com>
2025-03-06 11:41:13 +0200

    feat(Frontend): add Fotter component and integrate Blogs component into Home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';  
 import Hero from '../components/Here';
 import Blogs from '../components/Blogs';
+import Fotter from '../components/Fotter';
 const Home = () => {
     return (
       <>
         <Navbar /> 
         <Hero/> 
         <Blogs/> 
+        <Fotter/> 
       </>
     );
   };
