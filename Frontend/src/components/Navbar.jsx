ace1cb43f1abaea28d8f4eba0430d45505635091
Katleho14 <katlehomotloung394@gmail.com>
2025-03-06 08:56:58 +0200

    style(Navbar): update Navbar component with Tailwind CSS styling

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+  <div className="navbar flex items-center h-[100px] px-[100px] bg-red-500">
+
+  </div>
+    </>
   )
 }
 
