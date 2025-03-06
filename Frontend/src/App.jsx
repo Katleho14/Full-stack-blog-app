db650a78cd688d82d560ef633c922759620084ad
Katleho14 <katlehomotloung394@gmail.com>
2025-03-06 08:38:59 +0200

    add routing with BrowserRouter and define routes for Home, About, and NoPage components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
