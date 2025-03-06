cfeab90f3d203c8d2f97126c0955e8e6db98ad10
Katleho14 <katlehomotloung394@gmail.com>
2025-03-06 08:46:44 +0200

    wrap App component in a fragment and remove About route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,13 +6,15 @@ import NoPage from './pages/NoPage';
 
 const App = () => {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
+    </>
   )
 }
 
