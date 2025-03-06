e24cb11b604e22f47e8b8f49d01a0290ad638171
Katleho14 <katlehomotloung394@gmail.com>
2025-03-06 10:17:21 +0200

    feat(Home): integrate Blogs component into Home page layout

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar';  
 import Hero from '../components/Here';
+import Blogs from '../components/Blogs';
 const Home = () => {
     return (
       <>
         <Navbar /> 
         <Hero/> 
+        <Blogs/> 
       </>
     );
   };
