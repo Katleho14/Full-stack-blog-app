e73c844f166531371b87a7d1ca3b6b994a7f5e62
Katleho14 <katlehomotloung394@gmail.com>
2025-03-06 09:19:29 +0200

    feat(Home): add Navbar component to Home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar';  
 const Home = () => {
-  return (
-    <>
-    <Navbar/>
-    </>
-  )
-}
+    return (
+      <>
+        <Navbar />  
+      </>
+    );
+  };
+
 
 export default Home
